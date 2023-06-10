b2233b346b23445d0b627cb6ccfbdd8937c46543
ccacook98 <caleb_cook@outlook.com>
2023-06-10 23:10:39 +0000

    Added the /urls/:id endpoint as well as its associated template in views/urls_show.ejs.

--- express_server.js
+++ express_server.js
@@ -26,6 +26,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: /* What goes here? */ };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   const templateVars = { greeting: "Hello World!" };
   res.render("hello_world", templateVars);
