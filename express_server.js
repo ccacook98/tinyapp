46f9c70f955eb816e1e192a346b17dfc39ed47a1
ccacook98 <caleb_cook@outlook.com>
2023-07-12 07:16:18 +0000

    Implemented backend functionality for the login system.

--- express_server.js
+++ express_server.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
+app.use(cookieParser());
 const PORT = 8080; // default port 8080
 
 const generateRandomString = function() {
   //Generate a random six-character string. There's no good way to do this in JS without resorting to the use of a complex one-liner like this. Sorry...
   return Math.round((Math.pow(36, 6 + 1) - Math.random() * Math.pow(36, 6))).toString(36).slice(1);
-}
+};
 
 //Enable the EJS view engine
 app.set("view engine", "ejs");
@@ -30,7 +32,11 @@ app.get("/urls.json", (req, res) => {
 
 //Handler for displaying all registered URLs in human-readable HTML form
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+    // ... any other vars
+  };
   res.render("urls_index", templateVars);
 });
 
@@ -41,17 +47,25 @@ app.post("/urls", (req, res) => {
   //add a new property to urlDatabase with the random string as key and our URL as value
   urlDatabase[rndString] = req.body.longURL;
   //Once done, redirect the user to the page showing the details of their entry.
-  res.redirect('/urls/'+rndString);
+  res.redirect('/urls/' + rndString);
 });
 
 //Handler for the /urls/new form which allows new entries to be added.
 app.get("/urls/new", (req, res) => {
+/*  const templateVars = {
+    username: req.cookies["username"],
+    // ... any other vars
+  };*/
   res.render("urls_new");
 });
 
 //Display information for the URL referenced by the specified key.
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {
+    username: req.cookies["username"],
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -66,7 +80,8 @@ app.post("/urls/:id", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  res.cookie("username", req.params.username);
+  console.log(req.body);
+  res.cookie("username", req.body.username);
   res.redirect("/urls");
 });
 
@@ -81,12 +96,12 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/set", (req, res) => {
- const a = 1;
- res.send(`a = ${a}`);
+  const a = 1;
+  res.send(`a = ${a}`);
 });
 
 app.get("/fetch", (req, res) => {
- res.send(`a = ${a}`);
+  res.send(`a = ${a}`);
 });
 
 //Start listening on the specified port
