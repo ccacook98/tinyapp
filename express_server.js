873a83fe76e5639c343bd90050e7efe3786ec5dc
ccacook98 <caleb_cook@outlook.com>
2023-07-12 07:50:23 +0000

    Final commit for project; cleaned up some minor things.

--- express_server.js
+++ express_server.js
@@ -80,8 +80,12 @@ app.post("/urls/:id", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  console.log(req.body);
   res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
   res.redirect("/urls");
 });
 
